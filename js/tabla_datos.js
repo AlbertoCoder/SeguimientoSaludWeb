38ed5339e7db21f9e5d5154c8ae0a9cee9732d41
AlbertoCoder <albertocastur@gmail.com>
2024-04-10 12:57:55 +0200

    Reflexión de datos en la tabla.

--- js/tabla_datos.js
+++ js/tabla_datos.js
@@ -4,12 +4,14 @@ var idusuario_seleccionado = sessionStorage.getItem("id_usuario");
 var nomusuario_barra_nav;
 var it_fecha, it_peso, it_glucosa, it_o2, it_sist, it_diast, it_ppm, it_pasos, it_kms, it_cals;
 var btnInsertarRegistro, btnEliminarRegistro, btnLimpiarFormulario;
-var usuario;
+var tabla_datos;
 var baseDeDatos;
 
 window.onload = function() {
 
   nomusuario_barra_nav = document.getElementById("nomusuario_barra_nav");
+
+  tabla_datos = document.getElementById("tabla_datos");
 
   abrirDB("Seguimiento_Salud_Web", "success").then(db => {
 
@@ -20,6 +22,7 @@ window.onload = function() {
 
     });
 
+    generarRegistrosEntabla(baseDeDatos);
   }).catch(error => {
 
     console.error(error);
@@ -40,78 +43,28 @@ window.onload = function() {
   btnEliminarRegistro = document.getElementById("btnEliminarRegistro");
   btnLimpiarFormulario = document.getElementById("btnLimpiarFormulario");
 
-  btnInsertarRegistro.addEventListener("click", () => {
+}
 
-    idusuario_seleccionado = sessionStorage.getItem("id_usuario").split(" ")[0];
+function generarRegistrosEntabla(baseDeDatos) {
 
-    var nombre_usu = nomusuario_barra_nav.innerHTML.split(" ")[2];
-    var apellido1_usu = nomusuario_barra_nav.innerHTML.split(" ")[3];
-    var apellido2_usu = nomusuario_barra_nav.innerHTML.split(" ")[4];
+  leerTodosLosRegistros(baseDeDatos, "Mediciones").then(mapa_datos => {
 
-    var mediciones_prueba = {
+    for (let registro = 1; registro <= mapa_datos.size; registro++) {
 
-      N: parseInt(idusuario_seleccionado),
-      Nombre: nombre_usu,
-      Apellidos: `${apellido1_usu} ${apellido2_usu}`,
-      Fecha: it_fecha.value,
-      Peso: it_peso.value,
-      Glucosa: it_glucosa.value,
-      O2: it_o2.value,
-      Sist: it_sist.value,
-      Diast: it_diast.value,
-      PPM: it_ppm.value,
-      Pasos: it_pasos.value,
-      Kms: it_kms.value,
-      Cals: it_cals.value
+      const nueva_fila = document.createElement('tr');
+      tabla_datos.appendChild(nueva_fila);
+
+      for (let celda = 0; celda < 11; celda++) {
+
+        const nueva_celda = document.createElement('td');
+        nueva_celda.innerHTML = mapa_datos.get(registro).N;
+        nueva_fila.appendChild(nueva_celda);
+
+      }
+
     }
 
-    insertarRegistro(baseDeDatos, "Mediciones", mediciones_prueba).then(resultado => {
-
-      alert(`Registro agregado con fecha ${resultado.Fecha}`);
-
-    });
-
-    mostrarRegistroEnFormulario();
-
   });
 
-  btnLimpiarFormulario.addEventListener("click", limpiarFormulario);
-
 }
 
-function mostrarRegistroEnFormulario() {
-
-
-  leerTodosLosRegistros(baseDeDatos, "Mediciones").then(resultado => {
-
-    console.log("Valor: " + resultado.get(resultado.size).Fecha);
-    it_fecha.value = resultado.get(resultado.size).Fecha;
-    it_peso.value = resultado.get(resultado.size).Peso;
-    it_glucosa.value = resultado.get(resultado.size).Glucosa;
-    it_o2.value = resultado.get(resultado.size).O2;
-    it_sist.value = resultado.get(resultado.size).Sist;
-    it_diast.value = resultado.get(resultado.size).Diast;
-    it_ppm.value = resultado.get(resultado.size).PPM;
-    it_pasos.value = resultado.get(resultado.size).Pasos;
-    it_kms.value = resultado.get(resultado.size).Kms;
-    it_cals.value = resultado.get(resultado.size).Cals;
-
-  });
-
-
-}
-
-function limpiarFormulario() {
-
-  it_fecha.value = null;
-  it_peso.value = null;
-  it_glucosa.value = null;
-  it_o2.value = null;
-  it_sist.value = null;
-  it_diast.value = null;
-  it_ppm.value = null;
-  it_pasos.value = null;
-  it_kms.value = null;
-  it_cals.value = null;
-
-}
